0552b5e02923d947051488a95f5afca8f3b74160
itesh-singh <itesh5906@gmail.com>
2026-02-24 08:27:38 +0530

    feat(register): add custom validation for email and password confirmation

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,7 +10,7 @@ class RegistrationForm(forms.ModelForm):
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Confirm Password', 
     }))
-    class Meta:
+    class Meta:        
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
 
@@ -22,3 +22,14 @@ class RegistrationForm(forms.ModelForm):
         self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
         for field in self.fields:
             self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+    def clean(self):
+        cleaned_data = super(RegistrationForm, self).clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                "Password dose not match!"
+            )
