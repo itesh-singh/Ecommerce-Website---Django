a445c39acdbf0dd73e3e8f1a43a7f85148db886a
itesh-singh <itesh5906@gmail.com>
2026-02-25 09:21:20 +0530

    feat(auth): implement account activation setup with email verification
    - added account activation workflow
    - generate uid and token for email verification
    - prepare activation link logic
    - set inactive user until verification

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -137,3 +137,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: "danger",
 }
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'indianherocr@gmail.com'
+EMAIL_HOST_PASSWORD = 'IteshSinghKhoyal8009138841@gmail.com'
+EMAIL_USE_TLS = True
