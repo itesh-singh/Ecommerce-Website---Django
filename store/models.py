f73b05a32d35b0a2e9f350f9d58671982a756f88
itesh-singh <itesh5906@gmail.com>
2026-03-02 07:36:00 +0530

    feat(reviews): add average rating calculation and display rating score on product page

--- store/models.py
+++ store/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from category.models import Category
 from django.urls import reverse
 from accounts.models import Account
+from django.db.models import Avg
+
 
 # Create your models here.
 
@@ -22,6 +24,13 @@ class Product(models.Model):
 
     def __str__(self):
         return self.product_name
+    
+    def averageReview(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
     
 class VariationManager(models.Manager):
     def colors(self):
