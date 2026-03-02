c5fb39824e05cc65b9906b85bfcee0c583035bb4
itesh-singh <itesh5906@gmail.com>
2026-03-02 19:50:48 +0530

    feat(accounts): implement user profile model with admin thumbnail preview

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
-
+from .models import Account, UserProfile
+from django.utils.html import format_html
 
 class AccountAdmin(UserAdmin):
 
@@ -32,4 +32,24 @@ class AccountAdmin(UserAdmin):
     list_filter = ()
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, obj):
+        if obj.profile_picture:
+            return format_html(
+                '<img src="{}" width="30" height="30" style="border-radius:50%;" />',
+                obj.profile_picture.url
+            )
+        return "-"
+
+    thumbnail.short_description = "Profile Picture"
+
+    list_display = (
+        'thumbnail',
+        'user',
+        'city',
+        'state',
+        'country',
+    )
+
 admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
